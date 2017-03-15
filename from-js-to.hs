56bdee08c27d0f89c8530ff20d851ab20f7095d2
Patrick <patrick.tran06@gmail.com>
2017-03-14 22:16:43 -0700

    add a transition library

---
+++ from-js-to.hs
@@ -0,0 +1,22 @@
+-- quicksort.js
+-- const quicksort = function(arr) {
+--   if (arr.length === 0) {
+--     return [];
+--   }
+--   const [pivot, ...xs] = arr;
+--   const smallerSorted = quicksort(xs.filter((val) => {
+--     return val <= pivot;
+--   }));
+--   const biggerSorted = quicksort(xs.filter((val) => {
+--     return val > pivot;
+--   }));
+--   return smallerSorted.concat(pivot).concat(biggerSorted);
+-- };
+
+-- quicksort.hs
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+  let smallerSorted = quicksort [a | a <- xs, a <= x]
+      biggerSorted = quicksort [a | a <- xs, a > x]
+  in smallerSorted ++ [x] ++ biggerSorted
