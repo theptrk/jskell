5c3dd3570344615c47dc8eb9c3d9feb4d57a26b7
Patrick <patrick.tran06@gmail.com>
2017-05-22 20:28:24 -0700

    Add tree using Foldable

---
+++ cis194/examples/Tree.hs
@@ -0,0 +1,27 @@
+import qualified Foldable as F
+
+data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Show, Read, Eq)
+
+-- foldMap :: (Monoid m, Foldable t) => (a -> m) -> t a -> m
+
+instance F.Foldable Tree where
+    foldMap f Empty = mempty
+    foldMap f (Node x l r) = F.foldMap f l `mappend`
+                             f x           `mappend`
+                             F.foldMap f r
+
+testTree = Node 5
+            (Node 3
+                (Node 1 Empty Empty)
+                (Node 6 Empty Empty)
+            )
+            (Node 9
+                (Node 8 Empty Empty)
+                (Node 10 Empty Empty)
+           )
+
+treeToList :: Tree a -> [a]
+treeToList = foldMap (\x -> [x])
+
+main :: IO ()
+main = print treeToList testTree
